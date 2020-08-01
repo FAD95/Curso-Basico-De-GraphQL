51ab4ae575eb39900f4d64563d0000fc018c6f95
Fadith Escorcia <feescorciam@protonmail.com>
2020-07-31 22:18:56 -0500

    Argumentos

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
-const { buildSchema } = require('graphql')
+const { makeExecutableSchema } = require('graphql-tools')
 const { readFileSync } = require('fs')
 const { join } = require('path')
 const resolvers = require('./lib/resolvers')
@@ -11,9 +11,10 @@ const app = express()
 const port = process.env.PORT || 3000
 
 // definimos el esquema
-const schema = buildSchema(
-  readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
-)
+const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+const schema = makeExecutableSchema({typeDefs, resolvers})
+  
+
 
 app.use(
   '/api',
