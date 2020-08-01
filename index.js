51820fee2d110732b5a8b6169b29d016b3a7a596
Fadith Escorcia <feescorciam@protonmail.com>
2020-07-31 22:02:35 -0500

    Custom Types

--- index.js
+++ index.js
@@ -2,25 +2,18 @@
 
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
-const { graphql, buildSchema } = require('graphql')
+const { buildSchema } = require('graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 // definimos el esquema
-const schema = buildSchema(`
-    type Query {
-        hello: String
-    }
-`)
-
-// Configurar los resolvers
-const resolvers = {
-  hello: () => {
-    return 'hello world'
-  },
-
-}
+const schema = buildSchema(
+  readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+)
 
 app.use(
   '/api',
