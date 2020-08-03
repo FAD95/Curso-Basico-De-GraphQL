9799088a55e3b3c8425483f6b8bfb2cb6e3ec3a2
Fadith Escorcia <feescorciam@protonmail.com>
2020-08-03 00:14:03 -0500

    Preparando API para producción

--- index.js
+++ index.js
@@ -4,23 +4,27 @@ require('dotenv').config()
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const { makeExecutableSchema } = require('graphql-tools')
+const cors = require('cors')
 const { readFileSync } = require('fs')
 const { join } = require('path')
 const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.PORT || 3000
+const isDev = process.env.NODE_ENV !== 'production'
 
 // definimos el esquema
 const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
 const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+app.use(cors())
 
 app.use(
   '/api',
   graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true,
+    graphiql: isDev,
   })
 )
 
